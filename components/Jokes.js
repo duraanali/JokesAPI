0fd1e152d1f1261ba20cea4ef2fd3376ccf97f51
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2025-07-20 16:36:30 -0500

    Update Jokes.js

--- components/Jokes.js
+++ components/Jokes.js
@@ -1,61 +1,108 @@
-// 1. Call the api, using Axios
+/*
+<div class="joke-card">
+  <p class="joke-setup">Why are cats so good at video games?</p>
+  <p class="joke-delivery">They have nine lives.</p>
+</div>
+*/
 
-const API_URL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&type=twopart"
+// Let's create a function or a component that displays the joke
+
+function createJokeCard(jokeData) {
+  // Step 1: Create the main div
+  const mainDiv = document.createElement("div");
+  mainDiv.className = "joke-card";
+
+  // Step 2: Create the joke setup paragraph
+  const jokeSetup = document.createElement("p");
+  jokeSetup.className = "joke-setup";
+  jokeSetup.textContent = jokeData.setup;
+
+  // Step 2: Create the joke delivery paragraph
+  const jokeDelivery = document.createElement("p");
+  jokeDelivery.className = "joke-delivery";
+  jokeDelivery.textContent = jokeData.delivery;
+
+  // Step 3: Append
+  mainDiv.append(jokeSetup);
+  mainDiv.append(jokeDelivery);
+
+  return mainDiv;
+}
+
+// To access an object, you need to use either dot notation or bracket notation
+
+// const joke = {
+//     setup: "Why are cats so good at video games?",
+//     delivery: "They have nine lives."
+// }
+
+// const newJoke = {
+//     "error": false,
+//     "category": "Christmas",
+//     "type": "twopart",
+//     "setup": "What says Oh Oh Oh?",
+//     "delivery": "Santa walking backwards!",
+//     "flags": {
+//         "nsfw": false,
+//         "religious": false,
+//         "political": false,
+//         "racist": false,
+//         "sexist": false,
+//         "explicit": false
+//     },
+//     "id": 244,
+//     "safe": true,
+//     "lang": "en"
+// }
+
+
+// Promise based API
+ 
+
+// Create a function that calls the api
 
 function fetchJoke() {
-    return axios.get(API_URL)
-        .then((response) => response.data)
-        .catch((error) => console.log(error))
+  return axios.get("https://v2.jokeapi.dev/joke/Any?type=twopart&blacklistFlags=nsfw,explicit")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-// 2. Create a component/function to display the data from the API.
+function displayNewJokes() {
+  // How do we connect the function we created to index.html
+  // document.querySelector selects existing HTML elements (tags) with class, id or just the element by itself
+  const jokeContainer = document.querySelector(".joke-container");
 
-function createJokeCard(joke) {
+  // loading
+jokeContainer.innerHTML = "Loading..."
 
-    // Main div-ka aan ku xirmeyno
-    const mainDiv = document.querySelector(".joke-container")
 
-    // Wixii hore oo html ah oo ku jiray meesha ka bixi
-    mainDiv.innerHTML = ""
 
-    // Joke Card main div
-    const mainJokeDiv = document.createElement("div")
-    mainJokeDiv.className = "joke-card"
+  // now, get the new joke and connect it to the createJokeCard function
 
-    // Joke Setup p tag <p class="joke-setup">
-    const setUp = document.createElement("p")
-    setUp.className = "joke-setup"
-    setUp.textContent = joke.setup
+  fetchJoke().then((currentJoke) => {
+    console.log("currentJoke", currentJoke)
+      // clear the previous joke
+     jokeContainer.innerHTML = "";
 
-    // Delivery P tag <p class="joke-delivery">
-    const delivery = document.createElement("p")
-    delivery.className = "joke-delivery"
-    delivery.textContent = joke.delivery
-
-    // Isku xirka
-    mainDiv.append(mainJokeDiv)
-    mainJokeDiv.append(setUp)
-    mainJokeDiv.append(delivery)
-
-    return mainDiv
-
+    jokeContainer.append(createJokeCard(currentJoke));
+  });
 }
 
 
-// 3. Isku xir laba function oo kore, kadib ku xir HTML-ka ku jirto index.html
+// make the button click work
+// createElement is to create NEW element, not to get an existing element
+// If you want to create NEW element in the html and it doesn't exist, use document.createElement
+// If something is ALREADY in the HTML file, you use document.querySelector (or querySelectorAll) to select it
+// document.querySelector works EXACTLY like CSS.
 
-function displayJoke() {
-    fetchJoke()
-        .then((joke) => {
-            createJokeCard(joke)
-        })
-}
+const button = document.querySelector(".new-joke-btn")
 
+// addEventListener = a javascript method that allows you to handle EVENT (such as mouse click, keyboard press etc)
 
-// Button-ka ka shaqeysii
-const jokeButton = document.querySelector(".new-joke-btn")
+button.addEventListener("click", displayNewJokes)
 
-// Event Listner
-jokeButton.addEventListener("click", displayJoke)
-
-displayJoke()
+displayNewJokes();
